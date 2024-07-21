1fa53a09be709c38611529559dcf86c79e14d08e
dennisschuerholz <github.com@dennisschuerholz.de>
2024-07-21 10:46:58 +0000

    Deploying to gh-pages from @ dennisschuerholz/botc-bremen@a6b511630c0aa39cb81fd5b1202370c04ad192b7 🚀

--- js/scripts.js
+++ js/scripts.js
@@ -89,3 +89,20 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error(error);
         });
 });
+
+window.onscroll = () => {
+    let current = "";
+    for (const section of document.getElementsByTagName('section')) {
+        if (window.scrollY >= section.offsetTop - window.innerHeight * 0.45) {
+            current = section.id;
+        }
+    }
+
+    const active = document.querySelector('.navbar .nav-item .nav-link.active');
+    if (active != null && active.href !== `#${current}`) active.classList.remove('active');
+
+    if (active == null || active.href !== `#${current}`) {
+        const link = document.querySelector(`.navbar .nav-item .nav-link[href="#${current}"]`)
+        if (link != null) link.classList.add('active');
+    }
+};
