f6eac53fce5d7130c97984b427a96de9d3f0f3f0
dennisschuerholz <github.com@dennisschuerholz.de>
2025-06-16 06:50:40 +0000

    Deploying to gh-pages from @ dennisschuerholz/botc-bremen@abfbf8026e04e330136b5f1ea26fac2c3770026e 🚀

--- js/scripts.events.js
+++ js/scripts.events.js
@@ -14,8 +14,8 @@ function fillEvents() {
     const rootElement = document.getElementById('botc-event-widget');
     rootElement.innerHTML = '';
     const eventsCounter = document.getElementById('eventsCounter');
-    eventsCounter.innerHTML = `Seite ${Math.floor(startCounter/chunksize)+1} von ${Math.ceil(events.length/chunksize)}`;
-    const dateFormat = new Intl.DateTimeFormat('de-DE', {
+    eventsCounter.innerHTML = `${i18n[lang].page} ${Math.floor(startCounter/chunksize)+1} ${i18n[lang].of} ${Math.ceil(events.length/chunksize)}`;
+    const dateFormat = new Intl.DateTimeFormat(lang + "-DE", {
        weekday: 'short',
        year: 'numeric',
        month: '2-digit',
@@ -24,47 +24,17 @@ function fillEvents() {
        minute: '2-digit',
     });
     let counter = 0;
-    const langs = { 'de': 'Deutsch', 'en': 'Englisch'};
+    console.log("found " + events.length + " events");
     events.forEach(event => {
         counter++;
         if (counter <= startCounter || counter > startCounter+chunksize) {
             return;
         }
         const eventElement = document.createElement('li');
-        eventElement.classList.add('d-flex', 'list-group-item', 'justify-content-between', 'align-items-center', 'event');
-        eventElement.innerHTML = `
-            <div>
-                <b class="title d-inline-block mb-1">${event.title}</b>
-                ${event.languages.length > 0 ? `
-                    <div title="Kommunikationssprache">
-                        <i class="bi bi-translate"></i>
-                        <span class="ms-1">${event.languages.map(lc => langs[lc]).join(", ")}</span>
-                    </div>` : '' }
-                <div title="Start">
-                    <i class="bi bi-calendar-event"></i>
-                    <time class="ms-1" datetime="${event.start}">${dateFormat.formatRange(new Date(event.start), new Date(event.end))}</time>
-                </div>
-                <div title="Ort">
-                    <i class="bi bi-pin-map-fill"></i>
-                    ${event.locationUrl ? `<a href="${event.locationUrl}" target="_blank">` : ''}
-                    <span class="ms-1">${event.location || '<i>Noch nicht bestimmt</i>'}</span>
-                    ${event.locationUrl ? '</a>' : ''}
-                </div>
-                <div title="Storyteller">
-                    <i class="bi bi-person-workspace"></i>
-                    <span class="ms-1">${event.storyTellers.length > 0 ? event.storyTellers.join(", ") : '<i>Noch nicht bestimmt</i>'}</span>
-                </div>
-                ${event.extra ? `<div title="Weitere Informationen" class="extra"><i class="bi bi-info-lg"></i> <span class="ms-1">${event.extra}</span></div>` : ''}
-            </div>
-            ${event.url ? `<a href="${event.url}" target="_blank" title="Zur Anmeldung"><i class="bi bi-box-arrow-up-right"></i></a>` : ''}
-        `;
-        eventElement.addEventListener('mouseover', () => {
-            eventElement.classList.add('bg-light');
-        });
-        eventElement.addEventListener('mouseout', () => {
-            eventElement.classList.remove('bg-light');
-        });
         if (event.url) {
+            if (event.url.startsWith('https://events.botc-bremen.de/')) {
+                event.url += '?locale=' + lang;
+            }
             eventElement.addEventListener('click', (clickEvent) => {
                 if (clickEvent.target.parentElement.tagName === 'A') {
                     return;
@@ -73,8 +43,43 @@ function fillEvents() {
             });
             eventElement.classList.add('cursor-pointer');
         }
+        eventElement.classList.add('d-flex', 'list-group-item', 'justify-content-between', 'align-items-center', 'event');
+        eventElement.innerHTML = `
+            <div>
+                <b class="title d-inline-block mb-1">${event.title}</b>
+                ${event.languages.length > 0 ? `
+                    <div title="${i18n[lang].language}">
+                        <i class="bi bi-translate"></i>
+                        <span class="ms-1">${event.languages.map(lc => i18n[lang][lc]).join(", ")}</span>
+                    </div>` : '' }
+                <div title="${i18n[lang].datetime}">
+                    <i class="bi bi-calendar-event"></i>
+                    <time class="ms-1" datetime="${event.start}">${dateFormat.formatRange(new Date(event.start), new Date(event.end))}</time>
+                </div>
+                <div title="${i18n[lang].location}">
+                    <i class="bi bi-pin-map-fill"></i>
+                    ${event.locationUrl ? `<a href="${event.locationUrl}" target="_blank">` : ''}
+                    <span class="ms-1">${event.location || `<i>${i18n[lang].nyd}</i>`}</span>
+                    ${event.locationUrl ? '</a>' : ''}
+                </div>
+                <div title="${i18n[lang].storyteller}">
+                    <i class="bi bi-person-workspace"></i>
+                    <span class="ms-1">${event.storyTellers.length > 0 ? event.storyTellers.join(", ") : `<i>${i18n[lang].nyd}</i>`}</span>
+                </div>
+                ${event.extra ? `<div title="${i18n[lang].information}" class="extra"><i class="bi bi-info-lg"></i> <span class="ms-1">${event.extra}</span></div>` : ''}
+            </div>
+            ${event.url ? `<a href="${event.url}" target="_blank" title="${i18n[lang].registration}"><i class="bi bi-box-arrow-up-right"></i></a>` : ''}
+        `;
+        eventElement.addEventListener('mouseover', () => {
+            eventElement.classList.add('bg-light');
+        });
+        eventElement.addEventListener('mouseout', () => {
+            eventElement.classList.remove('bg-light');
+        });
         rootElement.appendChild(eventElement);
     });
+    console.log("after for");
+    console.log(rootElement);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -84,6 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const now = new Date();
             events = all_events.filter((event) => new Date(event.end) >= now);
             fillEvents(0);
+            console.log(document.getElementById('botc-event-widget'));
         })
         .catch(error => {
             console.error(error);
